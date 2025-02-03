0c400bf75794f3bf6c275a9cf2f4f31c8f22c29c
preethamak <167765989+preethamak@users.noreply.github.com>
2025-02-03 15:50:40 +0530

    Update constant.js

--- storage-app/pages/constant.js
+++ storage-app/pages/constant.js
@@ -1,5 +1,5 @@
 export const API_URL = "https://volta-rpc.energyweb.org/";
-export const PRIVATE_KEY ="c062f62696b83e68f3063c0d60246c498886670063b97b2e95e0b5282348480e";
+export const PRIVATE_KEY ="";
 export const contractAddress = "0x6d5bAEb49B64E39d3271372b4dE4f8985d94b016";
 export const contractAbi = [
 	{
